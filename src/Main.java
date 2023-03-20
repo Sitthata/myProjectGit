72deb33e36a9bf5599d8795046b88f0ecc0a5ca2
Sitthata <aprils96950@gmail.com>
2023-03-20 14:16:34 +0700

    Version 2 Update something

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git is fun");
         System.out.println("Version 1 New update from GitHub");
+        System.out.println("Version 2 New update from GitHub");
     }
 }
