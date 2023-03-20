b4b6e2dfc7c6baf2155299de6c9fb4f0d61f5ebc
Sitthata <aprils96950@gmail.com>
2023-03-20 14:25:50 +0700

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Git is fun");
         System.out.println("Version 1 New update from GitHub");
         System.out.println("Version 2 New update from GitHub");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Meow");
+        }
     }
 }
